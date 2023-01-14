910d233d092df1b4a47279594c5c16207c6612e1
kalebasi <itemk2323@gmail.com>
2023-01-14 13:48:48 +0300

    Commit All Files

---
+++ android/app/src/main/kotlin/com/example/cmmtbook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cmmtbook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/settings/config.dart
@@ -0,0 +1,7 @@
+
+class Config {
+  static const url="10.0.2.2:10000"; //Emülatörden çalışmak için IIS Express  1.
+  //  static const url="192.168.1.57:8031"; //Telefondan çalışmak için  IIS       2.
+ // static const url="10.0.2.2:3131"; //Emülatörden çalışmak için  WEBAPI
+//  static const url="192.168.1.45:10000"; //Telefondan çalışmak için  IIS
+}
